55e0ac7094e17b6d388b9b87fc928483e8d58c39
YixuanXu <yixuanxu94@outlook.com>
2024-01-06 18:26:28 +0800

    use top domain

--- app/cookie/[[...slug]]/route.ts
+++ app/cookie/[[...slug]]/route.ts
@@ -15,40 +15,40 @@ app.use('*',(c, next) => {
 })
 
 app.all('/cookie/test1/lax', (c) => {
-  setCookie(c, 'lax_cookie', 'test', {
+  setCookie(c, 'test1_lax_cookie', 'test1_lax', {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
     sameSite: 'Lax',
     secure: true,
-      domain: 'test1.yixuanxu.com'
+      domain: 'yixuanxu.com'
   })
   return c.text('test')
 })
 
 app.all('/cookie/test1/strict', (c) => {
-    setCookie(c, 'strict_cookie', 'test', {
+    setCookie(c, 'test1_strict_cookie', 'test1_strict', {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: 'Lax',
         secure: true,
-        domain: 'test1.yixuanxu.com'
+        domain: 'yixuanxu.com'
     })
     return c.text('test')
 })
 
 app.all('/cookie/test1/none', (c) => {
-    setCookie(c, 'none_cookie', 'test', {
+    setCookie(c, 'test1_none_cookie', 'test1_none', {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: 'None',
         secure: true,
-        domain: 'test1.yixuanxu.com'
+        domain: 'yixuanxu.com'
     })
     return c.text('test')
 })
 
 app.all('/cookie/test2/lax', (c) => {
-    setCookie(c, 'lax_cookie', 'test', {
+    setCookie(c, 'test2_lax_cookie', 'test', {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: 'Lax',
@@ -59,7 +59,7 @@ app.all('/cookie/test2/lax', (c) => {
 })
 
 app.all('/cookie/test2/none', (c) => {
-    setCookie(c, 'non_cookie', 'test', {
+    setCookie(c, 'test2_non_cookie', 'test', {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: 'None',
@@ -70,7 +70,7 @@ app.all('/cookie/test2/none', (c) => {
 })
 
 app.all('/cookie/test2/strict', (c) => {
-    setCookie(c, 'strict_cookie', 'test', {
+    setCookie(c, 'test2_strict_cookie', 'test', {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: 'Strict',
